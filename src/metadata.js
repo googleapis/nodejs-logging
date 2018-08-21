googleapis/nodejs-logging

commit 7feb300801506bfe0613cec281e5df28faa9d13a

refactor: update to latest google-auth-library and nodejs-common (#128)

src/metadata.js
@@ -123,21 +123,24 @@ Metadata.getGlobalDescriptor = function() {
  * @param {function} callback Callback function.
  */
 Metadata.prototype.getDefaultResource = function(callback) {
-  this.logging.auth.getEnvironment(function(err, env) {
-    if (env.IS_CONTAINER_ENGINE) {
-      Metadata.getGKEDescriptor(callback);
-    } else if (env.IS_APP_ENGINE) {
-      callback(null, Metadata.getGAEDescriptor());
-    } else if (env.IS_CLOUD_FUNCTION) {
-      callback(null, Metadata.getCloudFunctionDescriptor());
-    } else if (env.IS_COMPUTE_ENGINE) {
-      // Test for compute engine should be done after all the rest - everything
-      // runs on top of compute engine.
-      Metadata.getGCEDescriptor(callback);
-    } else {
-      callback(null, Metadata.getGlobalDescriptor());
-    }
-  });
+  this.logging.auth
+    .getEnv()
+    .then(env => {
+      if (env === 'CONTAINER_ENGINE') {
+        Metadata.getGKEDescriptor(callback);
+      } else if (env === 'APP_ENGINE') {
+        callback(null, Metadata.getGAEDescriptor());
+      } else if (env === 'CLOUD_FUNCTIONS') {
+        callback(null, Metadata.getCloudFunctionDescriptor());
+      } else if (env === 'COMPUTE_ENGINE') {
+        // Test for compute engine should be done after all the rest - everything
+        // runs on top of compute engine.
+        Metadata.getGCEDescriptor(callback);
+      } else {
+        callback(null, Metadata.getGlobalDescriptor());
+      }
+    })
+    .catch(callback);
 };
 
 module.exports = Metadata;

test/metadata.js
@@ -204,7 +204,7 @@ describe('metadata', function() {
 
   describe('getDefaultResource', function() {
     it('should get the environment from auth client', function(done) {
-      metadata.logging.auth.getEnvironment = function() {
+      metadata.logging.auth.getEnv = function() {
         done();
       };
 
@@ -220,11 +220,8 @@ describe('metadata', function() {
             return DESCRIPTOR;
           };
 
-          metadata.logging.auth.getEnvironment = function(callback) {
-            callback(null, {
-              IS_APP_ENGINE: true,
-              IS_COMPUTE_ENGINE: true,
-            });
+          metadata.logging.auth.getEnv = function() {
+            return Promise.resolve('APP_ENGINE');
           };
 
           metadata.getDefaultResource(function(err, defaultResource) {
@@ -243,11 +240,8 @@ describe('metadata', function() {
             return DESCRIPTOR;
           };
 
-          metadata.logging.auth.getEnvironment = function(callback) {
-            callback(null, {
-              IS_CLOUD_FUNCTION: true,
-              IS_COMPUTE_ENGINE: true,
-            });
+          metadata.logging.auth.getEnv = function() {
+            return Promise.resolve('CLOUD_FUNCTIONS');
           };
 
           metadata.getDefaultResource(function(err, defaultResource) {
@@ -266,10 +260,8 @@ describe('metadata', function() {
             successArg: {data: INSTANCE_ID},
           };
 
-          metadata.logging.auth.getEnvironment = function(callback) {
-            callback(null, {
-              IS_COMPUTE_ENGINE: true,
-            });
+          metadata.logging.auth.getEnv = function() {
+            return Promise.resolve('COMPUTE_ENGINE');
           };
 
           metadata.getDefaultResource(function(err, defaultResource) {
@@ -293,11 +285,8 @@ describe('metadata', function() {
             successArg: {data: CLUSTER_NAME},
           };
 
-          metadata.logging.auth.getEnvironment = function(callback) {
-            callback(null, {
-              IS_COMPUTE_ENGINE: true,
-              IS_CONTAINER_ENGINE: true,
-            });
+          metadata.logging.auth.getEnv = function() {
+            return Promise.resolve('CONTAINER_ENGINE');
           };
 
           metadata.getDefaultResource(function(err, defaultResource) {
@@ -321,13 +310,8 @@ describe('metadata', function() {
             return DESCRIPTOR;
           };
 
-          metadata.logging.auth.getEnvironment = function(callback) {
-            callback(null, {
-              IS_APP_ENGINE: false,
-              IS_CLOUD_FUNCTION: false,
-              IS_COMPUTE_ENGINE: false,
-              IS_CONTAINER_ENGINE: false,
-            });
+          metadata.logging.auth.getEnv = function() {
+            return Promise.resolve('NONE');
           };
 
           metadata.getDefaultResource(function(err, defaultResource) {

src/index.js
@@ -17,9 +17,9 @@
 'use strict';
 
 var arrify = require('arrify');
-var common = require('@google-cloud/common');
+var common = require('@google-cloud/common-grpc');
 var extend = require('extend');
-var googleAuth = require('google-auto-auth');
+var {GoogleAuth} = require('google-auth-library');
 var is = require('is');
 var pumpify = require('pumpify');
 var streamEvents = require('stream-events');
@@ -139,7 +139,7 @@ function Logging(options) {
   );
 
   this.api = {};
-  this.auth = googleAuth(options_);
+  this.auth = new GoogleAuth(options_);
   this.options = options_;
   this.projectId = this.options.projectId || '{{projectId}}';
 }

src/log.js
@@ -17,7 +17,7 @@
 'use strict';
 
 var arrify = require('arrify');
-var common = require('@google-cloud/common');
+var common = require('@google-cloud/common-grpc');
 var extend = require('extend');
 var is = require('is');
 var snakeCaseKeys = require('snakecase-keys');

src/sink.js
@@ -16,7 +16,7 @@
 
 'use strict';
 
-var common = require('@google-cloud/common');
+var common = require('@google-cloud/common-grpc');
 var extend = require('extend');
 var is = require('is');
 

test/entry.js
@@ -18,9 +18,9 @@
 
 var assert = require('assert');
 var extend = require('extend');
-var GrpcService = require('@google-cloud/common').GrpcService;
+var GrpcService = require('@google-cloud/common-grpc').GrpcService;
 var proxyquire = require('proxyquire');
-var util = require('@google-cloud/common').util;
+var util = require('@google-cloud/common-grpc').util;
 
 function FakeGrpcService() {}
 

test/index.js
@@ -21,7 +21,7 @@ var assert = require('assert');
 var extend = require('extend');
 var proxyquire = require('proxyquire');
 var through = require('through2');
-var util = require('@google-cloud/common').util;
+var util = require('@google-cloud/common-grpc').util;
 
 var v2 = require('../src/v2');
 
@@ -43,9 +43,9 @@ var fakePaginator = {
   },
 };
 
-var googleAutoAuthOverride;
-function fakeGoogleAutoAuth() {
-  return (googleAutoAuthOverride || util.noop).apply(null, arguments);
+var googleAuthOverride;
+function fakeGoogleAuth() {
+  return (googleAuthOverride || util.noop).apply(null, arguments);
 }
 
 var isCustomTypeOverride;
@@ -103,11 +103,13 @@ describe('Logging', function() {
 
   before(function() {
     Logging = proxyquire('../', {
-      '@google-cloud/common': {
+      '@google-cloud/common-grpc': {
         paginator: fakePaginator,
         util: fakeUtil,
       },
-      'google-auto-auth': fakeGoogleAutoAuth,
+      'google-auth-library': {
+        GoogleAuth: fakeGoogleAuth,
+      },
       './log.js': FakeLog,
       './entry.js': FakeEntry,
       './sink.js': FakeSink,
@@ -118,7 +120,7 @@ describe('Logging', function() {
   beforeEach(function() {
     extend(fakeUtil, originalFakeUtil);
 
-    googleAutoAuthOverride = null;
+    googleAuthOverride = null;
     isCustomTypeOverride = null;
     replaceProjectIdTokenOverride = null;
 
@@ -175,14 +177,14 @@ describe('Logging', function() {
       assert.deepEqual(logging.api, {});
     });
 
-    it('should cache a local google-auto-auth instance', function() {
-      var fakeGoogleAutoAuthInstance = {};
+    it('should cache a local GoogleAuth instance', function() {
+      var fakeGoogleAuthInstance = {};
       var options = {
         a: 'b',
         c: 'd',
       };
 
-      googleAutoAuthOverride = function(options_) {
+      googleAuthOverride = function(options_) {
         assert.deepEqual(
           options_,
           extend(
@@ -194,11 +196,11 @@ describe('Logging', function() {
             options
           )
         );
-        return fakeGoogleAutoAuthInstance;
+        return fakeGoogleAuthInstance;
       };
 
       var logging = new Logging(options);
-      assert.strictEqual(logging.auth, fakeGoogleAutoAuthInstance);
+      assert.strictEqual(logging.auth, fakeGoogleAuthInstance);
     });
 
     it('should localize the options', function() {

test/log.js
@@ -20,7 +20,7 @@ var assert = require('assert');
 var extend = require('extend');
 var prop = require('propprop');
 var proxyquire = require('proxyquire');
-var util = require('@google-cloud/common').util;
+var util = require('@google-cloud/common-grpc').util;
 
 var promisifed = false;
 var fakeUtil = extend({}, util, {
@@ -60,7 +60,7 @@ describe('Log', function() {
 
   before(function() {
     Log = proxyquire('../src/log.js', {
-      '@google-cloud/common': {
+      '@google-cloud/common-grpc': {
         util: fakeUtil,
       },
       './entry.js': Entry,

commit 7214ea7839e90db8247df282556a6735f0480fd5

fix(gke): correctly detect kubernetes engine
The value actually returned by google-auth-library is KUBERNETES_ENGINE.
https://github.com/google/google-auth-library-nodejs/blob/bad432105f5ad0203266e9c6b31d2235fc96534b/src/auth/envDetect.ts#L21

This regressed in https://github.com/googleapis/nodejs-logging/pull/128.

src/metadata.js
@@ -149,7 +149,7 @@ Metadata.prototype.getDefaultResource = function(callback) {
   this.logging.auth
     .getEnv()
     .then(env => {
-      if (env === 'CONTAINER_ENGINE') {
+      if (env === 'KUBERNETES_ENGINE') {
         Metadata.getGKEDescriptor(callback);
       } else if (env === 'APP_ENGINE') {
         callback(null, Metadata.getGAEDescriptor());

test/metadata.js
@@ -310,7 +310,7 @@ describe('metadata', function() {
           };
 
           metadata.logging.auth.getEnv = function() {
-            return Promise.resolve('CONTAINER_ENGINE');
+            return Promise.resolve('KUBERNETES_ENGINE');
           };
 
           metadata.getDefaultResource(function(err, defaultResource) {