ea560de86ac27d17bf5d851e7706f976da824f2e
Dominic Kramer <dominicdkramer@google.com>
2019-03-25 13:37:07 -0700

    chore: add code docs

--- src/metadata.ts
+++ src/metadata.ts
@@ -37,6 +37,11 @@ function zoneFromQualifiedZone(qualified: string): string|undefined {
  * @returns {object}
  */
 export function getCloudFunctionDescriptor() {
+  /**
+   * In GCF versions after Node 8, K_SERVICE is the preferred way to
+   * get the function name and GOOGLE_CLOUD_REGION is the preferred way
+   * to get the region.
+   */
   return {
     type: 'cloud_function',
     labels: {
