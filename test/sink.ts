de9795f874391f961e058f43c8175d1ceea8dd37
Benjamin Coe <bencoe@google.com>
2019-12-02 14:49:47 -0800

    chore: add test for http-request sample

--- test/sink.ts
+++ test/sink.ts
@@ -200,9 +200,10 @@ describe('Sink', () => {
       sink.getMetadata = async () => {
         throw error;
       };
-      sink
-        .setMetadata(METADATA)
-        .then(() => {}, err => assert.strictEqual(err, error));
+      sink.setMetadata(METADATA).then(
+        () => {},
+        err => assert.strictEqual(err, error)
+      );
     });
 
     it('should execute gax method', async () => {
