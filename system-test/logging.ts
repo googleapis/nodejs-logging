bebd37c5f942179e28d6e341e9ebf7609b762d8e
Stephen <stephenplusplus@users.noreply.github.com>
2020-02-04 15:18:25 -0500

    revert package.json and remove excess noise

--- system-test/logging.ts
+++ system-test/logging.ts
@@ -117,7 +117,6 @@ describe('Logging', () => {
       let numLogsDeleted = 0;
       for (const log of logsToDelete) {
         try {
-          console.log('Deleting log', log.name);
           await log.delete();
           numLogsDeleted++;
 
