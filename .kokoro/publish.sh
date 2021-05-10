40816a82df4c00dfb92b657803f52edb13ef1508
nicoleczhu <nicolezhu@google.com>
2021-05-10 00:14:49 -0700

    fix: add publish script again

--- .kokoro/publish.sh
+++ .kokoro/publish.sh
@@ -28,4 +28,31 @@ NPM_TOKEN=$(cat $KOKORO_GFILE_DIR/secret_manager/npm_publish_token)
 echo "//wombat-dressing-room.appspot.com/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 
 npm install
+
+# Publish the default package
+npm publish --access=public --registry=https://wombat-dressing-room.appspot.com
+
+# Uglify /build to publish a smaller package
+for d in $(find ./build/ -maxdepth 8 -type d)
+do
+  pushd $d
+    for f in *.js; do
+      [ -f "$f" ] || break
+
+      if [ -f "$f.map" ]; then
+        # Keep original .ts source mappings
+        uglifyjs --source-map "content='$f.map', url='$f.map'" "$f" --output "$f"
+      else
+        uglifyjs "$f" --output "$f"
+      fi
+    done
+  popd
+done
+
+# Change and publish under package name `@google-cloud/logging-min`
+sed -i -e 's/"name": "@google-cloud\/logging"/"name": "@google-cloud\/logging-min"/' package.json
+if [ -f "package.json-e" ]; then
+  rm package.json-e
+fi
+
 npm publish --access=public --registry=https://wombat-dressing-room.appspot.com
