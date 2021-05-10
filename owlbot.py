fa41e2262b1289b925c72330a4e4ccad2cff5f61
nicoleczhu <nicolezhu@google.com>
2021-05-10 00:13:38 -0700

    chore: owlbot to ignore custom publish script

--- owlbot.py
+++ owlbot.py
@@ -20,6 +20,7 @@ import synthtool.languages.node as node
 node.owlbot_main(
     staging_excludes=[
         ".eslintignore", ".prettierignore", "src/index.ts", "README.md", "package.json",
+        ".kokoro/publish.sh",
         "system-test/fixtures/sample/src/index.js",
         "system-test/fixtures/sample/src/index.ts"],
     templates_excludes=[
