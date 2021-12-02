c4bb7a6b5bc6cc1b2f1ebf8e1592d6c1e4b0ba5d
losalex <alex.losovsky@gmail.com>
2021-12-02 00:10:14 -0800

    fix: Correct a comment in sample regarding asynchronous log writing

--- samples/logs.js
+++ samples/logs.js
@@ -57,7 +57,8 @@ async function writeLogEntry(logName) {
     // Asynchronously batch write the log entries
     await log.write([text_entry, json_Entry]);
 
-    // Asynchronously let the logging library dispatch logs
+    // Let the logging library dispatch logs asynchronously without 
+    // awaiting for operation to complete
     log.write(text_entry);
 
     console.log(`Wrote to ${logName}`);
