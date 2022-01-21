0210e2e757808790156115362ac12c32985f34b6
losalex <alex.losovsky@gmail.com>
2022-01-21 13:03:39 -0800

    Add test for capital upper case letter

--- test/utils/log-common.ts
+++ test/utils/log-common.ts
@@ -100,12 +100,14 @@ describe('Log Common', () => {
         projectId: 'id',
         fooBarBaz: 'foobar',
         some_other: 'value',
+        AnotherOne: 'another',
       };
       const result = snakecaseKeys(labels);
       assert.deepStrictEqual(result, {
         project_id: 'id',
         foo_bar_baz: 'foobar',
         some_other: 'value',
+        _another_one: 'another',
       });
     });
   });
