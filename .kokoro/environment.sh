ecd02485c75babc8a661a57e28de3a87c39b72a9
Daniel Sanche <d.sanche14@gmail.com>
2023-01-23 17:09:41 -0800

    removed python version from script

--- .kokoro/environment.sh
+++ .kokoro/environment.sh
@@ -43,7 +43,6 @@ git submodule update --init --recursive
 cd "env-tests-logging/"
 export ENV_TEST_PY_VERSION=3.7
 echo "using python version: $ENV_TEST_PY_VERSION"
-python3 --version
 
 # Disable buffering, so that the logs stream through.
 export PYTHONUNBUFFERED=1
