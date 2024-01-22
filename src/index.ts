dc3e4da92ae1213552c89c8ffc68f56b453faaf9
cindy-peng <cindypeng@google.com>
2024-01-22 13:43:39 -0800

    feat: export LogSeverity for type enforcement

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ const version = require('../../package.json').version;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const v2 = require('./v2');
 
-import {Entry, LogEntry} from './entry';
+import {Entry, LogEntry, LogSeverity} from './entry';
 import {
   MonitoredResource,
   Severity,
@@ -1522,6 +1522,7 @@ export {Log};
  */
 export {Severity};
 export {SeverityNames};
+export {LogSeverity};
 
 export {assignSeverityToEntries};
 export {formatLogName};
